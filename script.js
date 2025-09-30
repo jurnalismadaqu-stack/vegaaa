cb7e494b2145f694715e718d671dd41f8aea09f3
vegaaa <jurnalismadaqu@gmail.com>
2025-09-30 14:14:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function() {
-alert('Thank you for visiting me!);
+     alert('Thank you for visiting me!);
 });
